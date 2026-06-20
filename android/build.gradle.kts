e67f9537ee3d6780c2744a743767b155a6b20ffe
bereke1t2 <bereketaschalew1212@gmail.com>
2026-06-20 11:07:36 +0000

    fix: suppress Java 8 obsolete-source warnings from plugin subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Force all plugin subprojects to compile with Java 11 so the
+// "source value 8 is obsolete" javac warnings disappear.
+subprojects {
+    afterEvaluate {
+        plugins.withId("com.android.library") {
+            extensions.configure<com.android.build.gradle.LibraryExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
+                }
+            }
+        }
+    }
+}
 subprojects {
     project.evaluationDependsOn(":app")
 }
